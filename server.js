dfd245bf000aa1ce571761153a973cfd9ecedebe
Nijantha Selvam <nijantha.selvam@idp.com>
2021-11-18 19:02:43 +0530

    location chaned

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Nijantha_Spacex'));
+app.use(express.static('./dist/space'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', { root: 'dist/space/' }),
 );
 
 // Start the app by listening on the default Heroku port
